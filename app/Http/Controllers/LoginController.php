9dffde6e17d3723597fbaf3be5be3a96757976e3
VanAnh2240 <22520040@gm.uit.edu.vn>
2026-03-14 04:27:09 +0700

    .

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\login;
 use App\Http\Controllers\Controller;
 use App\Models\Cart;
 use App\Models\Customer;
-use App\Models\Wishlist;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
