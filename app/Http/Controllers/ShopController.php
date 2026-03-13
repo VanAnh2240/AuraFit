7e42072b55ef947f91d493b4c2c68cc443d049d0
VanAnh2240 <22520040@gm.uit.edu.vn>
2026-03-14 04:03:58 +0700

    update

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\product;
+use App\Models\Product;
 use Illuminate\Support\Facades\DB;
 
 class ShopController extends Controller
